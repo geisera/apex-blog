b42233c47252067d342e8cc0be04c6d10422c6fe
Tony <geisera@gmail.com>
2025-05-02 13:43:18 -0400

    removed Discuss on X

--- layouts/PostLayout.tsx
+++ layouts/PostLayout.tsx
@@ -110,9 +110,9 @@ export default function PostLayout({ content, authorDetails, next, prev, childre
             <div className="divide-y divide-gray-200 dark:divide-gray-700 xl:col-span-3 xl:row-span-2 xl:pb-0">
               <div className="prose max-w-none pb-8 pt-10 dark:prose-invert">{children}</div>
               <div className="pb-6 pt-6 text-sm text-gray-700 dark:text-gray-300">
-                <Link href={discussUrl(path)} rel="nofollow">
+                {/* <Link href={discussUrl(path)} rel="nofollow">
                   Discuss on X...
-                </Link>
+                </Link> */}
                 {/* {` • `}
                 <Link href={editUrl(filePath)}>View on GitHub</Link> */}
               </div>
